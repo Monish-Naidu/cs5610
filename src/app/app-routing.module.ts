fc6bdaac1dabcf308c58c49d1ae459016cde8a63
Monish Naidu <monishnaidu18@gmail.com>
2019-02-08 22:04:29 -0800

    working new webpage and website edits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './views/usr/login/login.component';
+import {RegisterComponent} from './views/usr/register/register.component';
+import {ProfileComponent} from './views/usr/profile/profile.component';
 
-const routes: Routes = [];
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent },
+  {path: 'register', component: RegisterComponent},
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routing = RouterModule.forRoot(appRoutes);
